6e6e2719e71aa59c58ea6db5695def82fe56c079
dj0le <gstar12@hotmail.com>
2024-05-03 15:52:20 +0200

    fixing more issues

--- static/overview.js
+++ static/overview.js
@@ -4,23 +4,23 @@ let mechId = parseInt(currentUrl.split('/').pop());
 let cardsLength = 83;
 
 navigation.forEach((btn) => {
-  btn.addEventListener('click', () => {
-    fetchNew(mechId);
+	btn.addEventListener('click', () => {
+		fetchNew(mechId);
 
-    function fetchNew(mechId) {
-      let direction = 1;
-      if (btn.id === 'right') {
-        mechId = (mechId + direction) % cardsLength;
-      } else {
-        mechId = (mechId - direction + cardsLength) % cardsLength;
-      }
-      fetch(`${mechId}`).then((response) => {
-        if (response.ok) {
-          window.location = `${mechId}`;
-        } else {
-          fetchNew(mechId);
-        }
-      });
-    }
-  });
+		function fetchNew(mechId) {
+			let direction = 1;
+			if (btn.id === 'right') {
+				mechId = (mechId + direction) % cardsLength;
+			} else {
+				mechId = (mechId - direction + cardsLength) % cardsLength;
+			}
+			fetch(`${mechId}`).then((response) => {
+				if (response.ok) {
+					window.location = `${mechId}`;
+				} else {
+					fetchNew(mechId);
+				}
+			});
+		}
+	});
 });
